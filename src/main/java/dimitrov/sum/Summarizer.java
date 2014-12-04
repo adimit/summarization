a8c90cdfc48039967ef9ed5f7a62b6d58892088c
Aleksandar Dimitrov <aleks.dimitrov@gmail.com>
2014-12-04 23:13:00 +0100

    Get some nice logging going on.

--- src/main/java/dimitrov/sum/Summarizer.java
+++ src/main/java/dimitrov/sum/Summarizer.java
@@ -7,10 +7,18 @@ import java.util.ArrayList;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.bridge.SLF4JBridgeHandler;
 
 public class Summarizer {
 	private static final Logger log = LoggerFactory.getLogger(Summarizer.class);
+
 	public static void main(String[] args) {
+		// initialize
+
+		// Install the JUL-to-SLF4J bridge. This will handle the UIMA logs.
+		SLF4JBridgeHandler.removeHandlersForRootLogger();
+		SLF4JBridgeHandler.install();
+
 		// get file set
 
 		// make documents
