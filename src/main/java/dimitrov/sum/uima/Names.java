17ca51b488acd69cbec0607275aadba7dc4df36e
Aleksandar Dimitrov <aleks.dimitrov@gmail.com>
2015-01-18 11:38:57 +0100

    Properly configure TermFrequency AE.

--- src/main/java/dimitrov/sum/uima/Names.java
+++ src/main/java/dimitrov/sum/uima/Names.java
@@ -4,7 +4,10 @@ package dimitrov.sum.uima;
  * Created by aleks on 05/12/14.
  */
 public final class Names {
-    private Names() { }
+    public static final String TERM_FREQUENCY_TYPE_PARAMETER = "dimitrov.sum.TermType";
+    public static final String TERM_FREQUENCY_FEATURE_PARAMETER = "dimitrov.sum.TermFrequencyFeature";
+    public static final String TERM_SURFACE_FEATURE_PARAMETER = "dimitrov.sum.TermSurfaceFeature";
+    public static final String TERM_OBSERVATIONS_FEATURE_PARAMETER = "dimitrov.sum.TermObservationsFeature";
 
-    public static final String TFIDF_FEATURE_PARAMETER = "dimitrov.sum.uima.TfidfFeature";
+    private Names() { }
 }
